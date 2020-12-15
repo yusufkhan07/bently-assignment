e1f8dedfcd034a59ba36d578305c581bda51c677
Yousuf Khan <yusufkhanjee@gmail.com>
2020-12-15 05:26:35 +0500

    feat(task1): add slider to change background color of card

--- src/App.js
+++ src/App.js
@@ -3,30 +3,49 @@ import { useState } from 'react'
 
 import { ColorPicker } from './components/color-picker/color-picker'
 import { Box } from './components/box/box'
+import { SliderPicker } from 'react-color'
 
 function App() {
 	const [color, setColor] = useState('red')
+	const [bgColor, setBgColor] = useState('#7ECBA2')
 
-	const colorChanged = e => {
-		setColor(e.target.value)
+	const colorChanged = color => {
+		setColor(color)
+	}
+
+	const bgColorChanged = color => {
+		setBgColor(color.hex)
 	}
 
 	return (
 		<div className='App'>
-			<section class='hero is-primary'>
+			<section
+				class='hero'
+				style={{
+					backgroundColor: bgColor,
+				}}
+			>
 				<div class='hero-body'>
 					<div class='container'>
 						<h1 class='title'>Select a color from the drop-down</h1>
 						<div>
 							<ColorPicker
 								defaultColor={color}
-								colorChanged={colorChanged}
+								colorChanged={e => colorChanged(e.target.value)}
 							></ColorPicker>
 						</div>
 
 						<div>
 							<Box color={color}></Box>
 						</div>
+					</div>
+					<div class='container'>
+						<h1 class='title'>Try a new Background Color</h1>
+
+						<SliderPicker
+							color={bgColor}
+							onChangeComplete={bgColorChanged}
+						/>
 					</div>
 				</div>
 			</section>
