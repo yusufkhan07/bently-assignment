f23d5783300841aec9f509b2c624aca61745fce8
Yousuf Khan <yusufkhanjee@gmail.com>
2020-12-15 09:32:18 +0500

    refactor(app): remove unused code and type fix

--- src/components/color-picker/index.js
+++ src/components/color-picker/index.js
@@ -1,6 +1,6 @@
 export const ColorPicker = props => {
 	return (
-		<div className="select is-large">
+		<div className='select is-large'>
 			<select
 				name='colors'
 				id='color-picker'
@@ -10,7 +10,7 @@ export const ColorPicker = props => {
 				<option value='red'>Red</option>
 				<option value='blue'>Blue</option>
 				<option value='green'>Green</option>
-				<option value='black'>black</option>
+				<option value='black'>Black</option>
 			</select>
 		</div>
 	)
